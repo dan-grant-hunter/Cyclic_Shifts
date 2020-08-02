82bf815992b60bf7d08935be2b85acc51633313d
Dan Grant-Hunter <dan.granthunter@gmail.com>
2020-08-02 21:30:07 +0200

    Added initial functions

--- Cyclic_Shifts.py
+++ Cyclic_Shifts.py
@@ -1,1 +1,30 @@
-#
+# ---------------------------------------------------------------------------
+# Cyclic Shifts
+# ---------------------------------------------------------------------------
+
+# Get input from the user for Text
+def get_text():
+    pass
+
+# Get input from the user for String
+def get_string():
+    pass
+
+
+# Work out if T contains a cyclic shift of S
+def cyclic_shift():
+    pass
+
+
+# Return output
+def text_contains_cyclic_shift():
+    pass
+
+
+# Main Loop
+def main_loop():
+    pass
+
+
+# Replay Prompt
+
